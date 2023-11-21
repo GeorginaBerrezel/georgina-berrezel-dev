89ef5e945a8cf1afbe381798a50d88e47526b346
Georgina <88046437+GeorginaBerrezel@users.noreply.github.com>
2023-11-21 12:06:50 +0100

    Starting create project page and styling cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,30 +4,43 @@ import PropTypes from 'prop-types';
 import Button from '../Buttons/Button';
 import './Card.scss';
 
-function Card({ imagePath, description, iconPath }) {
+function Card({ title, titleExtend, imagePath, description, iconPath }) {
     return (
-        <div className="card">
-            <div className="card-image">
-                <img
-                    src={imagePath}
-                    alt="Project"
-                    className="card-image-background"
-                />
-                <div className="card-image-icon">
-                {iconPath && (
-                    <i className={iconPath} aria-hidden="true"></i>
+        <div>
+            <div className="card-title">
+                {title && (
+                    <p>{title}{String.fromCharCode(160)}</p>
                 )}
-                </div>
+                <p style={{ color: '#607B96', fontWeight:'100' }}> // {titleExtend}</p>
             </div>
-            <div className="card-text">
-                <p style={{ color: '#607B96' }}>{description}</p>
-                <Button type="cta-default">Voir plus</Button>
+
+            <div className="card">
+                <div className="card-image">
+                    <img
+                        src={imagePath}
+                        alt="Project"
+                        className="card-image-background"
+                    />
+                    <div className="card-image-icon">
+                        {iconPath && (
+                            <i className={iconPath} aria-hidden="true"></i>
+                        )}
+                    </div>
+                </div>
+                <div className="card-text">
+                    <p style={{ color: '#607B96' }}>{description}</p>
+                    <div className="card-button">
+                        <Button type="cta-default">view-project</Button>
+                    </div>
+                </div>
             </div>
         </div>
     );
 }
 
 Card.propTypes = {
+    title: PropTypes.string, // Nouvelle propriété pour le titre
+    titleExtend: PropTypes.string, // Nouvelle propriété pour le titre
     imagePath: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     iconPath: PropTypes.string, // Propriété optionnelle pour l'icône
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -4,21 +4,44 @@ import React from 'react';
 import Card from '../../components/Card/Card';
 import kasaLogementImage from '../../assets/img/kasa-logement.png';
 import lesPetitsPlats from '../../assets/img/les-petits-plats.png';
+import ohMyFood from '../../assets/img/oh-my-food.png';
+import gameOn from '../../assets/img/game-on.png';
+
+import './Projects.scss';
 
 function Projects() {
     return (
-     <div>
-            <h2>My Projects</h2>
+     <div className="container-project">
+         <div className="content-project">
             <Card
+                title="Project 1"
+                titleExtend="_react-app"
                 imagePath={kasaLogementImage}
                 description="React project! Lorem ipsum dolor sit amet."
                 iconPath="ri-reactjs-line"
             />
             <Card
+                title="Project 2"
+                titleExtend="_js-algoritm"
                 imagePath={lesPetitsPlats}
                 description="Algorithm project! Lorem ipsum dolor sit amet."
                 iconPath="ri-javascript-fill"
             />
+             <Card
+                 title="Project 3"
+                 titleExtend="_scss-animation"
+                 imagePath={ohMyFood}
+                 description="Css project with some animation"
+                 iconPath="ri-css3-fill"
+             />
+             {/*<Card*/}
+             {/*    title="Project 4"*/}
+             {/*    titleExtend="_js-form"*/}
+             {/*    imagePath={gameOn}*/}
+             {/*    description="First steps in JS world"*/}
+             {/*    iconPath="ri-javascript-fill"*/}
+             {/*/>*/}
+         </div>
      </div>
     );
 }
